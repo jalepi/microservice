193648bd591c7e876e8b5d19c0660b9ff0c389de
jalep <jalep@DESKTOP-7RCB1ST>
2020-02-17 21:09:15 +0100

    Setup Swashbuckle swagger

--- MicroService.Api/Startup.cs
+++ MicroService.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace MicroService.Api
 {
@@ -26,6 +27,15 @@ namespace MicroService.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo 
+                {
+                    Title = "Micro Service API", 
+                    Version = "v1",
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +55,13 @@ namespace MicroService.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Micro Service API V1");
             });
         }
     }
