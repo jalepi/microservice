30f4e217a44cc0a488334b38a4acf7a2fa9ed043
poajai04 <jaime.pinheiro@andritz.com>
2020-02-18 17:55:26 +0100

    versioning and routing

---
+++ MicroService.Api/V1/Controllers/SalesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MicroService.Api.V1.Controllers
+{
+    [ApiController]
+    public class SalesController : ControllerBase
+    {
+        private Services.ISalesService _salesService;
+
+        public SalesController(Services.ISalesService salesService)
+        {
+            this._salesService = salesService;
+        }
+
+        [HttpPost(Routes.Sales.PostItem)]
+        [Produces(typeof(string))]
+        public async Task<IActionResult> PostItem([FromBody] Models.SalesItem salesItem, CancellationToken cancellationToken)
+        {
+            var id = await this._salesService.AddSalesItemAsync(salesItem, cancellationToken);
+            return Ok(id);
+        }
+
+        [HttpGet(Routes.Sales.GetTotalRevenuesPerDay)]
+        [Produces(typeof(Models.TotalRevenuePerDay[]))]
+        public async Task<IActionResult> GetTotalRevenuesPerDay(CancellationToken cancellationToken)
+        {
+            var query = await this._salesService.GetTotalRevenuesPerDayAsync(cancellationToken);
+            return Ok(query);
+        }
+
+        [HttpGet(Routes.Sales.GetTotalRevenuesPerSalesItem)]
+        [Produces(typeof(Models.TotalRevenuePerSalesItem[]))]
+        public async Task<IActionResult> GetTotalRevenuesPerSalesItem(CancellationToken cancellationToken)
+        {
+            var query = await this._salesService.GetTotalRevenuesPerSalesItemAsync(cancellationToken);
+            return Ok(query);
+        }
+
+        [HttpGet(Routes.Sales.GetTotalSalesItemsPerDay)]
+        [Produces(typeof(Models.TotalSalesItemPerDay[]))]
+        public async Task<IActionResult> GetTotalSalesItemsPerDay(CancellationToken cancellationToken)
+        {
+            var query = await this._salesService.GetTotalSalesItemsPerDayAsync(cancellationToken);
+            return Ok(query);
+        }
+    }
+}
---
+++ MicroService.Api/V1/Routes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroService.Api.V1
+{
+    static public class Routes
+    {
+        public const string Base = "api/v1";
+
+        static public class Sales
+        {
+            public const string PostItem = Base + "/sales/item";
+            public const string GetTotalRevenuesPerDay = Base + "/sales/revenue-per-day";
+            public const string GetTotalRevenuesPerSalesItem = Base + "/sales/revenue-per-item";
+            public const string GetTotalSalesItemsPerDay = Base + "/sales/item-per-day";
+        }
+    }
+}
