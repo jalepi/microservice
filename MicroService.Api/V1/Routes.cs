656a7bafdda4ada2476eea3cc5d33c753f0d38ca
poajai04 <jaime.pinheiro@andritz.com>
2020-02-18 19:13:58 +0100

    cleanup - 6:45 so far

--- MicroService.Api/V1/Routes.cs
+++ MicroService.Api/V1/Routes.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MicroService.Api.V1
+﻿namespace MicroService.Api.V1
 {
     static public class Routes
     {
--- MicroService.Api/V1/Schema/CreateSalesItem.cs
+++ MicroService.Api/V1/Schema/CreateSalesItem.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Runtime.Serialization;
-using System.Threading.Tasks;
 
 namespace MicroService.Api.V1.Schema
 {
