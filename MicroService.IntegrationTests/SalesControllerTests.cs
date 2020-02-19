4a52846ef00fbbcf0c674597991b0c57502a7e82
poajai04 <jaime.pinheiro@andritz.com>
2020-02-19 14:03:04 +0100

    Added integration test project and basic test

---
+++ MicroService.IntegrationTests/SalesControllerTests.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Http.Extensions;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MicroService.IntegrationTests
+{
+    public class SalesControllerTests
+    {
+        private readonly HttpClient client;
+
+        public SalesControllerTests()
+        {
+            var factory = new WebApplicationFactory<Api.Startup>();
+            this.client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task BasicTest()
+        {
+            var requestValue = new Api.V1.Schema.CreateSalesItem
+            {
+                ArticleNumber = "abc123",
+                DateTime = DateTime.Now,
+                SalesPrice = 100.0M,
+            };
+            var requestJson = Newtonsoft.Json.JsonConvert.SerializeObject(requestValue);
+            var httpContent = new StringContent(requestJson, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(Api.V1.Routes.Sales.PostItem, httpContent);
+
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+            var responseValue = Newtonsoft.Json.JsonConvert.DeserializeObject<Api.V1.Schema.UpdateSalesItem>(responseJson);
+
+            Assert.False(String.IsNullOrEmpty(responseValue.Id));
+            Assert.Equal(requestValue.ArticleNumber, responseValue.ArticleNumber);
+            Assert.Equal(requestValue.DateTime, responseValue.DateTime);
+            Assert.Equal(requestValue.SalesPrice, responseValue.SalesPrice);
+
+
+        }
+    }
+}
